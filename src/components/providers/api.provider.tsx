d20a46c3cfe0168b689e8a03d1c0e3e411ecaaf3
Piotr Zieliński <piotr.zielinski@sidnet.pl>
2023-07-05 15:16:21 +0200

    For presentation purposes, disable setting stale time to cached data

--- src/components/providers/api.provider.tsx
+++ src/components/providers/api.provider.tsx
@@ -4,11 +4,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-    },
-  },
+  // defaultOptions: {
+  //   queries: {
+  //     staleTime: 1000 * 60 * 5,
+  //   },
+  // },
 });
 
 export default function ApiProvider({ children }: { children: React.ReactNode }) {
